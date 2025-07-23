d391d8fc0184c5e2d1095301b2521dc8b82697f4
Muhamad Anas Mustopa <muhamadanasmustopa1112@gmail.com>
2025-07-23 13:18:23 +0700

    feat : edit command

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,9 +2,26 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-
+/**
+ * @swagger
+ * /api/data:
+ *   get:
+ *     summary: Get all users
+ *     responses:
+ *       200:
+ *         description: Success
+ */
 router.get('/data', userController.getUsers);
 
+/**
+ * @swagger
+ * /api/gender-stats:
+ *   get:
+ *     summary: Get all gender statistics
+ *     responses:
+ *       200:
+ *         description: Success
+ */
 router.get('/data/gender-stats', userController.getGenderStats);
 
 module.exports = router;
